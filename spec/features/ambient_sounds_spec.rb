615a465cf78b0f9de333e73963f03d7c7fcba226
Ransom <ransom.byers@gmail.com>
2017-05-17 10:39:37 -0700

    Add test for sound on by default

--- spec/features/ambient_sounds_spec.rb
+++ spec/features/ambient_sounds_spec.rb
@@ -5,8 +5,18 @@ RSpec.feature 'Ambient sound indicator/selector', :type => :feature do
   scenario 'User visits any page' do
     visit '/'
 
-    expect(page).to have_selector(:link_or_button, 'Ambient Sound')
+    expect(page).to have_selector(:link_or_button, 'ambient sound')
   end
+
+  scenario 'Ambient sound should be enabled by default' do
+    visit '/'
+
+    expect(page).to have_selector(:link_or_button, 'sound-on')
+  end
+
+
+
+
 end
 
 
